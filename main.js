6197f8433aed081436d4ef63eeecee67b34b5363
iGuest <kberg721@hotmail.com>
2014-12-02 10:10:46 -0800

    working on email form

--- main.js
+++ main.js
@@ -9,3 +9,17 @@ $(document).ready(function() {
   });
  
 });
+
+var url = 'https://api.parse.com/1/classes/';
+
+angular.module('EmailApp', [])
+    .config(function($httpProvider) {
+    	$httpProvider.defaults.headers.common['X-Parse-Application-Id'] = 'wRO7sKRzS0HiGREF7Hele3LrJpocvYtdFyPYE072';
+        $httpProvider.defaults.headers.common['X-Parse-REST-API-Key'] = '2fcVE9FWvU2ZG8YAOf5gAIdA4AlbOSr8JgCHoXXm';
+    })
+
+    .controller('EmailController', function($scope, $http) {
+
+
+
+    });
