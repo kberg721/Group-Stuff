3fe5d661973e7b8ef136f5b2bc61a86eddf5c3b4
iGuest <kberg721@hotmail.com>
2014-12-02 10:11:55 -0800

    minor update

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
  
 });
 
-var url = 'https://api.parse.com/1/classes/';
+var url = 'https://api.parse.com/1/classes/emails';
 
 angular.module('EmailApp', [])
     .config(function($httpProvider) {
@@ -19,6 +19,45 @@ angular.module('EmailApp', [])
     })
 
     .controller('EmailController', function($scope, $http) {
+
+    	$scope.refreshEmails = function() {
+            $scope.loading = true;
+            $http.get(url)
+                .success(function(responseData) {
+                    $scope.comments = responseData.results;
+                })
+                .error(function(err) {
+                    console.log(err);
+                })
+                 .finally(function() {
+                    $scope.loading = false;
+                });
+        };
+
+        $scope.refreshEmails();
+
+        //initialize email object
+        $scope.newEmail = {done: false};
+
+        //adds new email from form
+    	$scope.addComment = function(comment) {
+            $scope.adding = true; //begin loading animation
+            $scope.none = false; //now a comment exists
+            comment.score = 0; //default comment score
+            $http.post(url + 'comments', comment)
+                .success(function(responseData) {
+                    comment.objectId = responseData.objectId;
+                    $scope.comments.push(comment);
+                    //clear form and create new comment object
+                    $scope.newComment = {done: false};
+                })
+                .error(function(err) {
+                	console.log(err);
+                })
+                .finally(function() {
+                    $scope.adding = false; //end loading animation
+                });
+        };
 
 
 
